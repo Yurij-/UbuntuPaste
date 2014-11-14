f3aa5a518c131690ec575057a81d69b2baf80932
Kevin Hu <hxy9243@gmail.com>
2014-11-14 00:33:55 -0500

    adding to urlrequest, more extensible

--- urlrequest.py
+++ urlrequest.py
@@ -1,40 +1,48 @@
 # -*- coding:utf-8 -*-
+# 
+# This file defines the RequestURL class,
+# which posts data to the server for response
+#
+# Author: Kevin Hu
 
 import requests
 
-URL_REQUEST = "http://paste.ubuntu.com"
-ARGKEYS = ["id_poster",
-           "id_syntax",
-           "id_content"]
+REQUEST_URL = {"ubuntu": "http://paste.ubuntu.com"}
 
 class RequestURL():
     """
     The class to send request url
     and receive response
     """
-
-    def __init__(self, content):
+    def __init__(self, server, content):
         """
         init function
         param: CONTENT - map from ARGKEYS to content
         """
+        if (server not in REQUEST_URL.keys()):
+            print("No such service " + server)
+            return
+        self.url = REQUEST_URL[server]
         self.content = content
 
        
     def send(self):
         """
         send request to server
-        return: request from server
+        return: request object from server
         """
-        r = requests.post(URL_REQUEST, data = self.content)
+        self.resp = requests.post(self.server, data = self.content)
 
-        return r
+        return self.resp
 
 
 def test():
+    """
+    Testcase for urlrequest class
+    """
     content = {"poster":"hello",
                "syntax":"python",
-               "content":"hello"}
+               "content":"import os\n\nos.listdir('.')\n"}
 
     req = RequestURL(content)
     resp = req.send()
@@ -46,5 +54,5 @@ def test():
             print (h.url)
 
 
-
-test()
+if __name__ == "__main__":
+    test()
